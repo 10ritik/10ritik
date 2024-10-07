1c9ebf4490d3bbce8f2014b5586649ad3ec0c85b
10ritik <93140180+10ritik@users.noreply.github.com>
2024-10-07 11:12:45 +0530

    Add files via upload

---
+++ Input_until_odd.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n=1;
+    while(n){
+        int x;
+        if (x%2==0){
+            cout<<"Input Your Number"<<endl;
+            cin>>x;
+        }
+        else
+            break;
+    }
+    cout<<"Program Successfully Terminated";
+}
