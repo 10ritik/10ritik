f01d1c770927dc810bde3b0a18af3e660a2e05e5
10ritik <93140180+10ritik@users.noreply.github.com>
2024-10-18 19:01:02 +0530

    Add files via upload

---
+++ Ques4.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+    float m=3.0;
+    switch((int)m)
+    {
+        case 1:
+                printf("Binary");
+                break;
+        case 2:
+                printf("Ultra");
+                break;
+        case 3:
+                printf("Binary Ultra");
+                break;
+    }
+    return 0;
+}
